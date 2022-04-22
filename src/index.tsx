64ef40329e15771f11aca1727714edaf6bc2899a
songmin <tft0720@gmail.com>
2022-04-22 19:35:51 +0900

    styled set

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import { RecoilRoot } from 'recoil';
+import GlobalStyles from './styles';
+import { ThemeProvider } from 'styled-components';
+import { initialTheme } from './theme';
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
   <React.StrictMode>
-    <App />
+    <RecoilRoot>
+      <ThemeProvider theme={initialTheme}>
+        <App />
+        <GlobalStyles />
+      </ThemeProvider>
+    </RecoilRoot>
   </React.StrictMode>
 );
---
+++ src/theme.ts
@@ -0,0 +1,7 @@
+import { DefaultTheme } from 'styled-components';
+
+export const initialTheme: DefaultTheme = {
+  bgColor: '#81ecec',
+  boardColor: '#DADFE9',
+  cardColor: '#ffffff',
+};
